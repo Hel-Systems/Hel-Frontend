6a3124f8ff8c7f8fd79b282e27e9971707ea3cee
BC-macair014 <bc-macair014@BC-macair014s-MacBook-Air.local>
2021-12-09 17:50:40 +0200

    registrarion form sends fetch request

---
+++ register.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const form = document.querySelector('.registration');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  if (form.elements['password'].value == form.elements['confirm'].value) {
+    fetch('http://localhost:8080/api/users', {
+      method: 'POST',
+      body: JSON.stringify({
+        firstname: form.elements['firstname'].value,
+        surname: form.elements['surname'].value,
+        email: form.elements['email'].value,
+        password: form.elements['password'].value,
+        role: form.elements['radio'].value,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+      .then(response => response.json())
+      .then(json => console.log(json));
+  }
+});
