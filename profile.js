d0690f761a7b85a541874884e24bbfa916362b6a
BC-macair014 <bc-macair014@BC-macair014s-MacBook-Air.local>
2021-12-12 13:51:04 +0200

    profile js is done. test 1

--- profile.js
+++ profile.js
@@ -1,33 +1,144 @@
 'use strict';
 
 let orders = [];
-let nameInput = document.querySelector('#name-input');
+let firstnameInput = document.querySelector('#firstname-input');
+let lastnameInput = document.querySelector('#lastname-input');
 let emailInput = document.querySelector('#email-input');
 let numberInput = document.querySelector('#number-input');
-let addressInput = document.querySelector('#address-input');
+// let addressInput = document.querySelector('#address-input');
 
+let currentPasswordInput = document.querySelector('#current-password-input');
+let newPasswordInput = document.querySelector('#new-password-input');
+let confirmPasswordInput = document.querySelector('#confirm-password-input');
 
-
-if (window.localStorage.getItem("current-user") !== '') {
+// load data of profile if current user is logged in
+if (window.localStorage.getItem("current-user")) {
   fetch(`http://localhost:8080/api/users/email/${window.localStorage.getItem("current-user")}`, {
     method: 'GET',
     headers: {
       "Content-type": "application/json; charset=UTF-8"
     }
   })
-    .then(response => {
-      if (response.status == 200) {
-        response.json();
-      }
-      else {
-        console.log('response status is not 200');
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      if (data !== undefined) {
+        localStorage.setItem("user-id", data["id"]);
+        firstnameInput.value = data["firstname"];
+        lastnameInput.value = data["surname"];
+        emailInput.value = data["email"];
+        // numberInput.value = data[""];
+      } else {
+        window.location.href = "/login.html";
       }
     })
-    .then(data => {
-      nameInput.value = data.firstname;
-      emailInput.value = data.email;
-    });
+} else {
+  window.location.href = "/login.html";
 }
+
+//load orders created by a user
+if (window.localStorage.getItem("current-user")) {
+  fetch(`http://localhost:8080/api/orders/user/${localStorage.getItem('user-id')}}`, {
+    method: 'GET',
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      let newRowHtml;
+      if (data !== undefined) {
+        for (i = 0; i < data.length; i++) {
+          newRowHtml += `<div class="order-table-row" id="${data[i].userId}"><div class="order-id">${data[i].id}</div><div class="order-description">${data[i].description}</div><div class="order-from">${data[i].adressFrom}</div><div class="order-to">${data[i].adressTo}</div><div class="order-status">${data[i].status}</div></div>`;
+        }
+        document.querySelector('.order-table').innerHTML = newRowHtml;
+      } else if (data == null) {
+        alert('You have not created any orders!');
+      } else {
+        console.log('Could not load orders of the user');
+      }
+    })
+} else {
+  window.location.href = "/login.html";
+}
+
+
+
+// update profile info request
+// document.querySelector('#save-profile').addEventListener('click', () => {
+//   if (firstnameInput.value !== '' && lastnameInput.value !== '' && emailInput.value !== '' && numberInput.value !== '') {
+//     fetch('http://localhost:8080/api/users', {
+//       method: 'POST',
+//       body: JSON.stringify({
+//         firstname: firstnameInput.value,
+//         surname: lastnameInput.value,
+//         email: emailInput.value,
+//         password: numberInput.value,
+//       }),
+//       headers: {
+//         "Content-type": "application/json; charset=UTF-8"
+//       }
+//     })
+//       .then(response => {
+//         if (response.status == 200) {
+//           window.location.href = "/profile.html"
+//         } else {
+//           console.log('profile is not updated!');
+//         }
+//       })
+//   } else {
+//     alert('Not all fields are filled!');
+//   }
+// });
+
+// change password request
+// document.querySelector('#save-password').addEventListener('click', () => {
+//   if (currentPasswordInput.value !== '' && newPasswordInput.value !== '') {
+//     if (newPasswordInput.value === confirmPasswordInput.value) {
+//       fetch(`http://localhost:8080/api/users/${localStorage.getItem('user-id')}`, {
+//         method: 'PATCH',
+//         body: JSON.stringify({
+//           currentpassword: currentPasswordInput.value,
+//           newpassword: newPasswordInput
+//         }),
+//         headers: {
+//           "Content-type": "application/json; charset=UTF-8"
+//         }
+//       })
+//         .then(response => response.json())
+//         .then(data => {
+//           console.log(data);
+//         })
+//     } else {
+//       alert('New password and its confirrmation are not the same!');
+//       currentPasswordInput.value = '';
+//       newPasswordInput.value = '';
+//       confirmPasswordInput.value = '';
+//     }
+//   }
+// });
+
+// delete profile request
+// document.querySelector('#delete-profile').addEventListener('click', () => {
+//   if (currentPasswordInput.value !== '' && newPasswordInput.value !== '') {
+//     fetch(`http://localhost:8080/api/users/${localStorage.getItem('user-id')}`, {
+//       method: 'DELETE',
+//       headers: {
+//         "Content-type": "application/json; charset=UTF-8"
+//       }
+//     })
+//       .then(response => {
+//         if (response.status == 200) {
+//           window.localStorage.removeItem('current-user');
+//           window.localStorage.removeItem('user-id');
+//           window.location.href = "/index.html";
+//         } else {
+//           alert('The password is not correct!');
+//         }
+//       })
+//   }
+// });
 
 // showing main menu options 
 document.querySelector('#my-orders').addEventListener('change', () => {
@@ -45,8 +156,6 @@ document.querySelector('#profile').addEventListener('change', () => {
     document.querySelector('#profile-title').textContent = 'Profile';
   }
 });
-
-
 // showing different sub-menu options
 document.querySelector('#edit-profile').addEventListener('change', () => {
   if (document.querySelector('#edit-profile').checked == true) {
@@ -56,7 +165,6 @@ document.querySelector('#edit-profile').addEventListener('change', () => {
     document.querySelector('.delete-profile').classList.add('hide');
   }
 });
-
 document.querySelector('#password').addEventListener('change', () => {
   if (document.querySelector('#password').checked == true) {
     document.querySelector('.edit-inputs').classList.add('hide');
@@ -65,7 +173,6 @@ document.querySelector('#password').addEventListener('change', () => {
     document.querySelector('.delete-profile').classList.add('hide');
   }
 });
-
 document.querySelector('#delete-profile').addEventListener('change', () => {
   if (document.querySelector('#delete-profile').checked == true) {
     document.querySelector('.edit-inputs').classList.add('hide');
